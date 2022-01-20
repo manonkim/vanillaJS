8df79ee3bc4c5ffcb0a40503ef9bb128247851a0
WY K <wyk@WYui-MacBookPro.local>
2022-01-21 02:18:19 +0900

    Add : click event / toggle

--- nomad1.js
+++ nomad1.js
@@ -1,9 +1,25 @@
-const winWidth = window.innerWidth;
+const h1 = document.querySelector("h2");
 
-function colorChange() {
-  if (winWidth > "600") {
-    body.style.borderColor = "blue";
+const handleTitle = () => {
+  const stylecolor = h1.style.color;
+  let newcolor;
+  if (stylecolor === "blue") {
+    newcolor = "tomato";
+  } else {
+    newcolor = "blue";
   }
-}
+  h1.style.color = newcolor;
+};
 
-window.addEventListener("resize", colorChange);
+const handleTitle1 = () => {
+  const clickedClass = "clicked";
+  h1.classList.contains(clickedClass)
+    ? h1.classList.remove(clickedClass)
+    : h1.classList.add(clickedClass);
+};
+
+const handleTitle2 = () => {
+  h1.classList.toggle("clicked");
+};
+
+h1.addEventListener("click", handleTitle);
